cb4fd0ae9f0ba9ed7c49eca6823704d8e2588476
Barry Williams <bazzawill@gmail.com>
2023-02-20 09:46:49 +1030

    Barrys neovim kickstart

--- init.lua
+++ init.lua
@@ -239,6 +239,8 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', "ft", vim.cmd.Lex, { desc = '[F]ile [T]ree' })
+vim.keymap.set('n', "cb", vim.cmd.bd, { desc = '[C]lose [B]uffer' })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
